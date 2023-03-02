03a66a43f37e7da8455ddb77eadfa6df58e97108
BOCHO <ebocho1337@gmail.com>
2023-03-02 17:17:14 +0400

    finished all exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let sum = 0
+    if (start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number') {
+        return "ERROR"
+    }
+    else {
+        if (start < end) {
+            for (start; start <= end; start++) {
+                sum += start
+            }
+        }
+
+        else {
+            for (end; end <= start; end++)
+                sum += end
+        }
+
+        return sum
+    }
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (far) {
+  result = (far - 32) * 5 / 9
+  rounded = result.toFixed(1)
+  return parseFloat(rounded)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  result = (cel * 9 / 5) + 32
+  rounded = result.toFixed(1)
+  return parseFloat(rounded)
 };
 
 // Do not edit below this line
